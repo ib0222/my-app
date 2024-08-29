6b1946e5ba174a4d12fae02834a947809d9d87f0
ib0222 <ibrahim.ismayilzade221@gmail.com>
2024-08-30 01:26:56 +0400

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,13 @@
+import { View, ScrollView, SafeAreaView } from "react-native";
+import React from "react";
+import { Stack, useRouter } from "expo-router";
+
+
+
+
+
+const Layout = () => {
+  return <Stack />;
+};
+
+export default Layout;
